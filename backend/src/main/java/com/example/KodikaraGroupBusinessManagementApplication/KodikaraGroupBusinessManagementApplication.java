cd6f73da78bf4da6dfe219a24bfcc77372b8d0ec
rishindu973 <it24610795@my.sliit.lk>
2025-09-22 21:38:41 +0530

    Added Spring Boot backend to backend/ folder

---
+++ backend/src/main/java/com/example/KodikaraGroupBusinessManagementApplication/KodikaraGroupBusinessManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.KodikaraGroupBusinessManagementApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodikaraGroupBusinessManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodikaraGroupBusinessManagementApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/KodikaraGroupBusinessManagementApplication/KodikaraGroupBusinessManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KodikaraGroupBusinessManagementApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodikaraGroupBusinessManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
