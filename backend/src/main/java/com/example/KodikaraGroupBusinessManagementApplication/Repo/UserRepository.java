856f9f8d34085bcde714eaca7e46df1dc283f4cf
rishindu973 <it24610795@my.sliit.lk>
2025-10-20 11:51:15 +0530

    implemented get/delete sale by date

--- backend/src/main/java/com/example/KodikaraGroupBusinessManagementApplication/Repo/UserRepository.java
+++ backend/src/main/java/com/example/KodikaraGroupBusinessManagementApplication/Repo/UserRepository.java
@@ -1,4 +1,11 @@
 package com.example.KodikaraGroupBusinessManagementApplication.Repo;
 
-public interface UserRepository {
+import com.example.KodikaraGroupBusinessManagementApplication.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByUsername(String username);
 }
